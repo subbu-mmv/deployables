156f351d18e46cadd37fb93570b9aa19896fef6e
Subba Rao MMV <mohanmopidevi@gmail.com>
2026-09-02 13:32:35 +0530

    Old->archived | Updated additional section

---
+++ GK-Photography/prices.js
@@ -0,0 +1,58 @@
+const PRICES = {
+
+    dayRates: {
+        photographer: 8000,
+        videographer: 12000,
+    },
+
+
+    additional: {
+
+        drone: {
+            name: "Drone",
+            rate: 20000
+        },
+
+        ledWall: {
+            name: "LED Wall",
+            rate: 15000
+        },
+
+        youtubeLive: {
+            name: "YouTube Live",
+            rate: 5000
+        },
+
+        cinematicTraditionalVideo: {
+            name: "Cinematic / Traditional Video",
+            rate: 10000
+        },
+
+        dvds: {
+            name: "DVDs",
+            rate: 2000
+        },
+
+        albumSheets: {
+            name: "Album Sheets",
+            rate: 100
+        },
+        
+        reels: {
+            name: "Reels / Shorts / Teasers",
+            rate: 1000
+        },
+
+        iphoneReels: {
+            name: "iPhone Reels",
+            rate: 1000
+        },
+
+        teaser: {
+            name: "Teaser",
+            rate: 3000
+        },
+
+    }
+
+};
